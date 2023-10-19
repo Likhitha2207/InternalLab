49d79a6bec4cfbc9b2f351ad441b5a58d84ffda8
AdmiN <AdmiN@DESKTOP-I89KIRV>
2023-10-19 11:49:10 +0530

    committed changes in areas class

--- src/Areas.java
+++ src/Areas.java
@@ -1,11 +1,13 @@
 public class Areas {
 	void circle(int r) {
+		System.out.println(3.14*r*r);
 		
 	}
 	void rectangle(int l, int b) {
+		System.out.println(l*b);
 		
 	}
 	void triangle(int b, int h) {
-		
+		System.out.println(0.5*b*h);
 	}
 }
