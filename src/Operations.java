2b958692d97b5a314fe4867941941bc29a2c8794
AdmiN <AdmiN@erripuka>
2023-10-19 05:44:21 +0530

    2 nd commited by Nag

--- src/Operations.java
+++ src/Operations.java
@@ -1,14 +1,14 @@
 public class Operations {
 	void add(int a, int b) {
-		
+		System.out.println(a+b);
 	}
 	void sub(int a, int b) {
-		
+		System.out.println(a+b);
 	}
 	void mul(int a, int b) {
-		
+		System.out.println(a*b);
 	}
 	void div(int a, int b) {
-		
+		System.out.println(a/b);
 	}
 }
